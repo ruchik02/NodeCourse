9110ace9fded8bb070165d679042d450bcfe20a0
ruchik02 <ruchika02>
2022-02-14 13:19:12 +0530

    file created

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log("folder is created");
 // fs.writeFile('bio.txt',"hello, i'm ruchika sharma",(err,data)=>{
 //     console.log("task completed");
 // })
+
+fs.writeFile('./thapas/bio.txt',"hello,how are you ?",(err)=>{
+    console.log("file is created");
+})
