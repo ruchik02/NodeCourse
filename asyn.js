3790b314fcdbe9756d8f2399696db84a72f20b7d
ruchik02 <ruchika02>
2022-02-14 12:01:16 +0530

    asyn read file

--- asyn.js
+++ asyn.js
@@ -1,4 +1,7 @@
 const fs=require('fs');
-const data=fs.readFileSync('read.txt','UTF-8');
-console.log(data);
-console.log("After the data");
+// const data=fs.readFileSync('read.txt','UTF-8');
+// console.log(data);
+fs.readFile('read.txt','utf-8',(err,data)=>{
+   console.log(data);
+})
+console.log("after the data");
