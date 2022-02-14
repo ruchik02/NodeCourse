f749d120e4982a8b43d151cac9f86d25ecbdb1d0
ruchik02 <ruchika02>
2022-02-14 13:25:28 +0530

    append file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs=require('fs');
-fs.mkdir('thapas',(err)=>{
-console.log("folder is created");
-})
+// fs.mkdir('thapas',(err)=>{
+// console.log("folder is created");
+// })
 // fs.writeFile('bio.txt',"hello, i'm ruchika sharma",(err,data)=>{
 //     console.log("task completed");
 // })
 
-fs.writeFile('./thapas/bio.txt',"hello,how are you ?",(err)=>{
-    console.log("file is created");
+// fs.writeFile('./thapas/bio.txt',"hello,how are you ?",(err)=>{
+//     console.log("file is created");
+// })
+
+fs.appendFile('./thapas/bio.txt'," yeah i'm good what about you ?",()=>{
+    console.log('append file');
 })
