3364a66d6200c6d35949030d70eee2cee6e90402
ruchik02 <ruchika02>
2022-02-15 13:30:28 +0530

    changes in index and oper file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const {add,sub}= require('./oper');
+const {add,sub,mult}= require('./oper');
 // console.log(oper);
 console.log(add(5,5));
 console.log(sub(10,5));
+console.log(mult(10,5));
--- oper.js
+++ oper.js
@@ -4,6 +4,9 @@ const add=(a,b)=>{
 const sub=(a,b)=>{
      return a-b;
 }
+const mult=(a,b)=>{
+    return a*b;
+}
 // module.exports.add=add;
 // module.exports.sub=sub;
-module.exports={add,sub};
+module.exports={add,sub,mult};
