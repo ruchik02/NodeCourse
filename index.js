57c23ae3a1737b833ef6daef2e0c072266c9c2bb
ruchik02 <ruchika02>
2022-02-16 23:31:53 +0530

    json file

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ fs.writeFile('json1.json', jsonData,(err)=>{
     console.log('done');
 });
 fs.readFile("json1.json","utf-8",(err,data)=>{
-console.log(data);
+// console.log(data);
+const orgData=JSON.parse(data);
+console.log(orgData);
 })
