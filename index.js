220f10d327cf4850d6a0b01da26325e37fb2b058
ruchik02 <ruchika02>
2022-03-04 23:34:53 +0530

    weather-app-using-api

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+const port=8000;
+var requests = require("requests");
+
+
+const homeFile = fs.readFileSync("index.html", "utf-8");
+const replaceVal=(tempVal,orgVal)=>{
+    let temperature=tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  return temperature;
+}
+    const server = http.createServer((req, res) => {
+        if (req.url == "/") {
+          requests(
+            " https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=8f9e414cf0321f044b5212cf1b899d96"
+          )
+            .on("data", (chunk) => {
+              const objdata = JSON.parse(chunk);
+              const arrData = [objdata];
+              // console.log(arrData[0].main.temp);
+              const realTimeData = arrData
+                .map((val) => replaceVal(homeFile, val))
+                .join("");
+              res.write(realTimeData);
+              // console.log(realTimeData);
+            })
+            .on("end", (err) => {
+              if (err) return console.log("connection closed due to errors", err);
+              res.end();
+            });
+        } else {
+          res.end("File not found");
+        }
+      });
+server.listen(port,"127.0.0.1");
