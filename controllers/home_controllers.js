08afb7407898593949609b738cc88b61baeb1f97
ruchik02 <ruchika02>
2022-03-28 17:00:10 +0530

    create partials and layouts

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,4 +1,7 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Hello This is Codeial Page !</h1>');
+    // return res.end('<h1>Hello This is Codeial Page !</h1>');
+    return res.render('home', {
+        title: 'Home Page'
+    })
 }
 // module.exports.actionName=function(req,res)
--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -1,3 +1,6 @@
 module.exports.user=function(req,res){
-    return res.end('<h1>Hello Profile here !</h1>');
+    // return res.end('<h1>Hello Profile here !</h1>');
+    return res.render('profile', {
+        title: 'Profile user'
+    })
 }
