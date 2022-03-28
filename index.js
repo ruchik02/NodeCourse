2a4150184de61d853846709341368639bdf0e614
ruchik02 <ruchika02>
2022-03-28 16:02:57 +0530

    setup view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port=8000;
 // use express router
 // app.get('./route');
 app.use('/',require('./route'));
+// set up the view engine 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         // console.log('Error :',err);
