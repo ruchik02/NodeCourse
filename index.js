07cb11629316b210ab1b204a038d7cf78725a77c
ruchik02 <ruchika02>
2022-03-29 11:07:27 +0530

    link mongoose

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express=require('express');
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+// extract style and scripts from sub page into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.use(expressLayouts);
 // use express router
