9a32f264c0d197fd46ad3867c3d16ec51c85e902
ruchik02 <ruchika02>
2022-04-01 21:25:39 +0530

    path module

--- second.js
+++ second.js
@@ -1,11 +1,14 @@
 // console.log("exports :|",exports, "require :",require,"filename :"  ,__filename,__dirname);
 
-const os=require('os');
-console.log(os.freemem());
-console.log(os.homedir());
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.release());
-
+// const os=require('os');
+// console.log(os.freemem());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.release());
+const path = require('path');
+console.log(path.basename('C:\\temp\\hello.html'));
+console.log(path.dirname('C:\\temp\\hello.html'));
+console.log(path.extname(__filename));
 
 
