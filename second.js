19291fe2899ae957ce045b717717d7e62aea96e4
ruchik02 <ruchika02>
2022-04-01 19:55:05 +0530

    os module

---
+++ second.js
@@ -0,0 +1,11 @@
+// console.log("exports :|",exports, "require :",require,"filename :"  ,__filename,__dirname);
+
+const os=require('os');
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+
+
+
