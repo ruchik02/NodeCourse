c6f29fcd13676bbb33b5db5dd821181d76bfd45d
ruchik02 <ruchika02>
2022-04-03 16:35:21 +0530

    express

--- expressap.js
+++ expressap.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const path = require('path')
+const port = 3000;
+// const ruchiMiddleware=(res,req,next)=>{
+// console.log(req);
+// next();
+// }
+// express middleware
+app.use(express.static(path.join(__dirname,"public")));
+// app.use(ruchiMiddleware);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
-app.get('/about',(req,res)=>{
-    res.send('Hello Everyone!');
+app.get('/about/:name',(req,res)=>{
+    res.send('Hello Everyone!' + req.params.name);
 })
 app.get('/sending',(req,res)=>{
      res.send('This is send page');
 })
+app.get('/sendings',(req,res)=>{
+  // res.sendFile(path.join(__dirname,'index.html'));
+  // res.status(500);
+  res.json({
+    "Ruchika":34
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
