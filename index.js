2b502a42f51e96621b0b4c59651994ccfb912a35
ruchik02 <ruchika02>
2022-04-04 22:28:25 +0530

    send json data

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-   res.send("Hello World");
+    res.write("<h1>Hello world</h1>");
+    res.write("<p>Hello</p>");
+   res.send();
 });
 app.get('/about',(req,res)=>{
     res.send("Hello About");
@@ -12,7 +14,20 @@ app.get('/about',(req,res)=>{
     res.send("Hello home");
  });
  app.get('/contact',(req,res)=>{
-    res.send("Hello contact");
+    res.json([
+        {
+            id:1911981325,
+            name:"Ruchika"
+        },
+        {
+            id:1911981381,
+            name:"Anushka"
+        },
+        {
+            id:1911981381,
+            name:"samridhi"
+        }
+    ]);
  });
 
 app.listen(port,(req,res)=>{
