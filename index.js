6b18636e0bbc68f087129c5844889f19901254ea
ruchik02 <ruchika02>
2022-04-05 13:18:21 +0530

    customize your directory

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const express=require('express');
 const app=express();
 const port=3000;
  const staticPath=path.join(__dirname,'../public');
+ const templatePath=path.join(__dirname,'/view');
 
 // builtin middleware
-// app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 
 // to set the view engine 
 app.set('view engine', 'hbs');
+app.set("views", templatePath);
 
-app.get('',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('index',{
         title:"dynamic website"
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:"About website"
     });
 });
 
